d6b847ed4cbe6039500b2ef02eaf21bf0ab3f3dc
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-01-22 23:46:13 +0300

    add components

--- gkandemir/19-urun-islemleri-uygulamasi/src/App.vue
+++ gkandemir/19-urun-islemleri-uygulamasi/src/App.vue
@@ -1,13 +1,42 @@
 <script>
-export default {
+import Header from "./components/shared/Header.vue";
+import Footer from "./components/shared/Footer.vue";
 
-}
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  created() {
+    this.$store.dispatch("getTradeResult");
+    this.$store.dispatch("initApp");
+  },
+};
 </script>
 
 <style>
-
+body {
+  background-color: #a7a0bb5c;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-leave {
+}
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+}
 </style>
 
 <template>
-  
+  <div class="my-5">
+    <Header />
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <Footer />
+  </div>
 </template>
---
+++ gkandemir/19-urun-islemleri-uygulamasi/src/components/shared/Header.vue
@@ -0,0 +1,25 @@
+<script>
+export default {};
+</script>
+
+<style>
+</style>
+
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark fixed-top"
+    style="background-color: #563f7a"
+  >
+    <router-link class="navbar-brand" tag="a" to="/">Ürün Listesi</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link class="nav-item" to="/urun-cikisi" tag="li" active-class="active">
+          <a class="nav-link">Ürün Çıkışı</a>
+        </router-link>
+        <router-link class="nav-item" to="/urun-islemleri" tag="li" active-class="active">
+          <a class="nav-link">Ürün İşlemleri</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
