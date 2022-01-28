c8f95875280fd8159d61a0e05876ea4b8398cc20
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-01-28 21:18:00 +0300

    update components

---
+++ gkandemir/bolum-18-vuex/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button class="btn btn-success" @click="incrementCounter">+ Arttır</button>
+    <button class="btn btn-danger" @click="decrementCounter">- Azalt</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+export default {
+  methods : {
+    /* incrementCounter(){
+      //this.$emit("counterEvent", 1);
+      this.$store.commit("increaseCounter")
+    },
+    decrementCounter(){
+      //this.$emit("counterEvent", 1);
+      this.$store.commit("decreaseCounter")
+    }, */
+    ...mapMutations({
+      incrementCounter: 'increaseCounter',
+      decrementCounter: 'decreaseCounter'
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ gkandemir/bolum-18-vuex/src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p class="counter-container">Sayaç : {{ getDoubleCounter }}</p>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  //props : ["counter"]
+  computed: {
+    ...mapGetters(["getDoubleCounter", "getCounterClick"]),
+  },
+  /* {
+      counter(){
+        return this.$store.getters.getDoubleCounter
+      },
+      getCounterClick(){
+        return this.$store.getters.getCounterClick
+      }
+    } */
+};
+</script>
+<style scoped>
+.counter-container {
+  border: 1px solid #aa7f08;
+  padding: 10px 5px;
+  background-color: #fbbd08;
+  font-size: 20px;
+  color: #000;
+}
+</style>
