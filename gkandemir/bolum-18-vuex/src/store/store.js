eec5767705ad0202b1a14e54c68fba8bb9671275
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2022-01-28 21:17:52 +0300

    add store

---
+++ gkandemir/bolum-18-vuex/src/store/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    state: {
+        counter: 0
+    },
+    getters: {
+        getDoubleCounter(state){
+            return state.counter * 2
+        },
+        getCounterClick(state){
+            return state.counter + " kez tıklandı"
+        }
+    },
+    mutations: {
+        increaseCounter(state){
+            return state.counter += 1
+        },
+        decreaseCounter(state){
+            return state.counter -= 1
+        },
+    }
+})
